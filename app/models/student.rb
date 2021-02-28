ccd70d0d918ccac60c3615c13700db4676d2f7ac
Cori Blount <heycorihey@icloud.com>
2021-02-28 15:31:35 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
 class Student
+attr_accessor :first_name, :last_name
+
+ @@all = []
+
+def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+end 
+
+def self.all
+    @@all
+end
+
+def add_boating_test(name,status,instructor)
+    BoatingTest.new(self, name, status, instructor )
+end
+
+def all_instructors
+    self.all.find {|students| self.first_name == students.first_name}
+end
 
 end
